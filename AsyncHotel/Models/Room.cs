38251fb5419d6a2f625288a58b9e56b229e1fb4e
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-26 14:26:08 -0500

    added model for room table

---
+++ AsyncHotel/Models/Room.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncHotel.Models
+{
+    public class Room
+    {
+        public int Id { get; set; }
+
+        public string name { get; set; }
+
+        public Layout layout { get; set; }
+
+        public enum Layout
+        {
+            Studio,
+            OneBedroom,
+            TwoBedroom,
+                
+        }
+    }
+}

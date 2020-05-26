2c75c3bfd905f3948142c81059269b806dd1e97d
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-26 14:37:10 -0500

    added model for hotel

---
+++ AsyncHotel/Models/Hotel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncHotel.Models
+{
+    public class Hotel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string StreetAddress { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+
+        public string Phone { get; set; }
+    }
+}

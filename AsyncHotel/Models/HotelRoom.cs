0a0756ca9fe93da83f9a709dc252f2b321e85327
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-26 15:56:37 -0500

    added model for HotelRoom

---
+++ AsyncHotel/Models/HotelRoom.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncHotel.Models
+{
+    public class HotelRoom
+    {
+        public Hotel Hotel { get; set; }
+
+        public int HotelId { get; set; }
+
+        public int RoomNumber { get; set; }
+
+        public Room RoomId { get; set; }
+
+        public decimal Rate { get; set; }
+
+        public bool PetFriendly { get; set; }
+
+    }
+}

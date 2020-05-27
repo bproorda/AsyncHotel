68062bf1f284ceb304cf41b0ff66817162634ffb
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-27 18:35:58 -0500

    refactored hotelcontroller

---
+++ AsyncHotel/Data/Repositories/DatabaseHotelRepository.cs
@@ -0,0 +1,36 @@
+﻿using AsyncHotel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncHotel.Data.Repositories
+{
+    public class DatabaseHotelRepository : IHotelRepository
+    {
+        public Task<Hotel> DeleteHotel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Hotel>> GetAllHotels()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Hotel> GetOneHotel(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Hotel> SaveNewHotel(Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateHotel(int id, Hotel hotel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

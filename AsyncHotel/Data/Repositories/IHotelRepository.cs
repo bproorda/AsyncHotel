6ed6d6535bbe1ee1cd9d75313b4755aa7bac920e
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-27 17:55:49 -0500

    added IHotelRepository

---
+++ AsyncHotel/Data/Repositories/IHotelRepository.cs
@@ -0,0 +1,33 @@
+﻿using AsyncHotel.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncHotel.Data.Repositories
+{
+    public interface IHotelRepository
+    {
+         Task<IEnumerable<Hotel>> GetAllHotels();
+
+        Task<Hotel> GetOneHotel(int id);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="hotel"></param>
+        /// <returns>hotel exists or not</returns>
+        Task<bool> UpdateHotel(int id, Hotel hotel);
+
+
+        Task<Hotel> SaveNewHotel(Hotel hotel);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns> deleted hotel</returns>
+        Task<Hotel> DeleteHotel(int id);
+    }
+}

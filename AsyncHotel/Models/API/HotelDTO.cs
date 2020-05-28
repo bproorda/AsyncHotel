7237b7cc5177dbcb445fa0398415170a6a2888c8
Brennan Roorda <54748984+bproorda@users.noreply.github.com>
2020-05-27 22:11:43 -0500

    added hotelDTO

---
+++ AsyncHotel/Models/API/HotelDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncHotel.Models.API
+{
+    public class HotelDTO
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        //Navigation properites
+        public ICollection<HotelRoom> HotelRooms { get; set; }
+    }
+}
